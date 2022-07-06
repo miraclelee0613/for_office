37d2c3fa416f1d0a2b21ea5c98d7677c011e6c94
NT-350 <NT-350@DESKTOP-1JN9UEQ>
2022-07-06 12:03:03 +0900

    20220706 12:02 파일 생성

---
+++ src/office_exe/FileNaming.java
@@ -0,0 +1,39 @@
+package office_exe;
+import java.util.Scanner;
+
+public class FileNaming {
+	public static int[] cntUp(int num) {
+
+		int arr[] = new int[num];
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+
+		}
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("");
+		// 대분류		00
+		
+		// 소분류		01
+		
+		// 날짜		20220706
+		
+		// 워커코드	702
+		
+		// 시간대		11
+		
+		// 순서		01
+		
+		// 소음/소리 	NN/SS		
+		
+		
+		
+		
+		
+		
+	}
+}
